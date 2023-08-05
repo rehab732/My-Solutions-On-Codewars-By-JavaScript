2bdada3e0a6c49267399cc08fd89b1cc9faebcdc
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-08-05 03:29:43 +0300

    vowel count

--- 7 kyu/VowelCount.js
+++ 7 kyu/VowelCount.js
@@ -1,3 +1,33 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
 function getCount(str) {
-  return 0;
+  let count=0;
+  let vowels=['a','e','i','o','u'];
+  if(str === str.toLowerCase())
+  {
+    let newstr=str.split('');
+    for (let i=0;i<newstr.length;i++)
+    {
+      if(vowels.includes(newstr[i]))
+        count++;
+    }
+    return count;
+  }
+
+}
+
+//Sol 2#
+
+function getCountV2(str) {
+  let vowels='aeiou'
+  if(str === str.toLowerCase())
+  {
+    let newstr=str.split('');
+    return newstr.filter((letter)=>vowels.includes(letter)).length;
+  }
+
 }
